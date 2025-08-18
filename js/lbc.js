e8af0ab18b1c0b81845fbe1db90131504cd2749f
caominha215 <caominha215@gmail.com>
2025-08-18 09:28:45 +0700

    Update lbc.js

--- js/lbc.js
+++ js/lbc.js
@@ -41,7 +41,7 @@ app.controller('myCtrl', function($scope, $http) {
 			if (id) {
 				if(store_no == 3) 
 				{
-					s.server = "https://ed548c8d8243.ngrok-free.app";
+					s.server = "https://0be2a214e834.ngrok-free.app";
 				}
 				s.reportFilters = [];
 				s.receipt_sid = id
@@ -368,6 +368,7 @@ app.controller('myCtrl', function($scope, $http) {
 		});
 		
 });
+
 
 
 
